eb71220c9caee73f3fab22b76349f568afbafb85
Robertm339 <71312299+Robertm339@users.noreply.github.com>
2022-07-04 13:33:17 -0700

    Initial Commit

---
+++ DiceOff/DiceOffApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceOffApp.swift
+//  DiceOff
+//
+//  Created by Robert Martinez on 7/4/22.
+//
+
+import SwiftUI
+
+@main
+struct DiceOffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
